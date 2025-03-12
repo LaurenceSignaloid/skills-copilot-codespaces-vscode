7ea68eca8b7f1c9363d89584e428bd92ca78d7e6
Laurence Weir <laurence.weir@signaloid.com>
2025-03-12 11:18:22 +0000

    Third commit

--- comments.js
+++ comments.js
@@ -1,31 +1,3 @@
 // Create Web Server
-var express = require('express');
-var app = express();
-
-// Create a server
-var server = require('http').createServer(app);
-
-// Create a WebSocket server
-var io = require('socket.io').listen(server);
-
-// Create a connection
-io.sockets.on('connection', function(socket) {
-  console.log('Client connected');
-
-  // Receive a message
-  socket.on('message', function(message) {
-    console.log('Message received: ' + message);
-
-    // Send the message to all clients
-    io.sockets.emit('message', message);
-  });
-
-  // Disconnect
-  socket.on('disconnect', function() {
-    console.log('Client disconnected');
-  });
-});
-
-// Listen on port 8080
-server.listen(8080);
-console.log('Server running on http://localhost:8080');
+// Create Web Server
+// Create Web Server
