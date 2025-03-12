1159e16e547614aa1466b6b787b15f7cdfb8b3e3
Laurence Weir <laurence.weir@signaloid.com>
2025-03-12 11:20:16 +0000

    Third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,14 @@
-// Create Web Server
-// Create Web Server
-// Create Web Server
+// Create web server
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Create a route for comments
+app.get('/comments', (req, res) => {
+    res.send('This is the comments page');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
